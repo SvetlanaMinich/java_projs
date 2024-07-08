0233d7ab19717a0f2c6b63e0eafef26e90f2bdc1
Svetlana Minich <imsvetam@mail.ru>
2024-07-08 16:54:54 +0300

    learned some java

--- first_expirience/Main.java
+++ first_expirience/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello VSCode!\n");
+        System.out.println("Hello world!");
+        
+        final var CONST_TRY = 'H';
+        System.out.println(CONST_TRY);
+
+        int digitsOfNums = 123_456;
+        System.out.println(digitsOfNums);
+
+        String textBlock = """
+                Hello
+                World
+                """;
+        System.out.println(textBlock);
+
+        // Scanner in = new Scanner(System.in);
+        // System.out.println("Enter line");
+        // var numbFromConsole = in.nextLine();
+        // System.out.println(numbFromConsole);
+        // in.close();
+
+        int increment = digitsOfNums++;
+        System.out.println(increment);
+        System.out.println(digitsOfNums);
+
+        
     }
 }
